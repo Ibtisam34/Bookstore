6fdf24386114d85e1c5c60c391afcfb7d6780165
unknown <Riyanagwl@gmail.com>
2023-01-02 18:00:08 +0300

    modified:   src/pages/Categories.js

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,9 +1,10 @@
+import React from 'react';
+
 const Categories = () => (
-  <>
-    <div className="alert alert-warning">
-      <h2>Under construction</h2>
-    </div>
-  </>
+  <div>
+    <h1>Categories</h1>
+    <button type="button">Check Status</button>
+  </div>
 );
 
 export default Categories;
