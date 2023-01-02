958be71c38258af1f614ba9176bc1924898cd3b2
unknown <Riyanagwl@gmail.com>
2023-01-02 21:38:27 +0300

    modified:   src/pages/Categories.js

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div>
-    <h1>Categories</h1>
-    <button type="button">Check Status</button>
-  </div>
-);
+const Categories = () => {
+  const status = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+
+  const handleStatus = () => {
+    dispatch(checkStatus());
+  };
+
+  return (
+    <div>
+      <h1>Categories</h1>
+      <button type="button" onClick={() => handleStatus()}>Check Status</button>
+      <p>{status}</p>
+    </div>
+  );
+};
 
 export default Categories;
